ee0fd2f38529bf9702bda9783f2e26ad5a6e05bf
Brendh4 <brendhauchiha@gmail.com>
2023-11-07 17:35:26 +0000

    writes questions in array syntax

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,12 +1,20 @@
 //at least 3 questions, can use the ones in the mock up, should be stored into an array not separate objects/variables
 // some info shows on page, but not everything in the obj needs to be in the page
 
-// commonly used data types do NOT include:
-// 1 strings 2 booleans 3 alers 4numbers
-
-// Arrays in Javascript can be used to store:
-// 1 numbers and strings 2 other arrays 3 booleans 4 all of the above
-
-// the coniditon in an if/else statement is enclosed with ______
-// 1 quotes 2 curly brackets 3 parenthesis 4 square brackets 
-
+var questions = [
+    { 
+    question: "Commonly used data types do NOT include:",
+    answer: ["Strings", "Booleans", "Alerts", "Numbers"],
+    correctAnswer: 2 
+    },
+    {
+    question: "arrays in Javascript can be used to store:",
+    answer: ["Numbers and strings", "Other arrays", "Booleans", "All of the above"],
+    correctAnswer: 2
+    },
+    {
+    question: "The condition in an if/else statement is enclosed with _____",
+    answer: ["Quotes", "Curly brackets", "Paranthesis", "Square brackets"],
+    correctAnswer: 2
+    }
+]
